5ae5f70d32415899a8c6b4fa5bad125249bbff6e
Patrick Foley <pfoley@belvederetrading.com>
2018-04-24 15:53:06 -0500

    Initial MVC project files and basic layout

---
+++ MusicPlayer/MusicPlayer.Presentation.Web/Controllers/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicPlayer.Presentation.Web.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PlayPause()
+        {
+            return View();
+        }
+
+        public IActionResult SkipForward()
+        {
+            return View();
+        }
+
+        public IActionResult SkipBackward()
+        {
+            return View();
+        }
+
+        public IActionResult ToggleShuffle()
+        {
+            return View();
+        }
+    }
+}
---
+++ MusicPlayer/MusicPlayer.Presentation.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MusicPlayer.Presentation.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
